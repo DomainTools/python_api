4232e0842c41c7223bd5dcf3f199818c46a5d6a5
Timothy Crosley <tcrosley@domaintools.com>
2018-03-21 13:43:45 -0700

    Update requirements

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = ['requests']
 packages = ['domaintools']
 if sys.version_info[0] >= 3 and sys.version_info[1] >= 5:
     packages.append('domaintools_async')
-    requires.append('aiohttp==1.0.5')
+    requires.append('aiohttp==2.3.6')
 elif sys.version_info[0] == 2 and sys.version_info[1] <= 6:
     requires.extend(['ordereddict', 'argparse'])
 
