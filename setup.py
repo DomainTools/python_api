b7daab8b4c42adb093b49940032d3a6995545de4
Timothy Crosley <timothy.crosley@gmail.com>
2018-07-27 12:59:03 -0700

    Bump version to 0.2.3

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ except (IOError, ImportError, OSError, RuntimeError):
    readme = ''
 
 setup(name='domaintools_api',
-      version='0.2.2',
+      version='0.2.3',
       description="DomainTools' Official Python API",
       long_description=readme,
       author='DomainTools',
