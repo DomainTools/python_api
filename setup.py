DomainTools/python_api

commit 91fe5b615c90a814888db6461da783b05904b9d1

Update setup to require 3.5.2

setup.py
@@ -11,7 +11,7 @@ from setuptools.command.test import test as TestCommand
 
 requires = ['requests']
 packages = ['domaintools']
-if sys.version_info[0] >= 3 and sys.version_info[1] >= 5:
+if sys.version_info[0] >= 3 and sys.version_info[1] >= 5 and sys.version_info[2] >= 2:
     packages.append('domaintools_async')
     requires.append('aiohttp==3.4.4')
 elif sys.version_info[0] == 2 and sys.version_info[1] <= 6:

commit 08991b3178a2a8596c284116976c8df601e85539

Fix a regression introduced in 91fe5b615c90a814888db6461da783b05904b9d1 which prevented the installation of domaintools-async on python 3.7.0/3.7.1 (#28)

setup.py
@@ -11,10 +11,13 @@ from setuptools.command.test import test as TestCommand
 
 requires = ['requests']
 packages = ['domaintools']
-if sys.version_info[0] >= 3 and sys.version_info[1] >= 5 and sys.version_info[2] >= 2:
+
+major, minor, patch = sys.version_info
+
+if major >= 3 and (minor > 5 or (minor == 5 and patch >= 2)):
     packages.append('domaintools_async')
     requires.append('aiohttp==3.4.4')
-elif sys.version_info[0] == 2 and sys.version_info[1] <= 6:
+elif major == 2 and minor <= 6:
     requires.extend(['ordereddict', 'argparse'])
 
 MYDIR = path.abspath(os.path.dirname(__file__))