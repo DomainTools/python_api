b858af5c2c0410b05fa658f5ae3f69f6307d9a65
g-clef <spamblock@g-clef.net>
2020-12-28 16:54:26 -0500

    fix parsing of ip whois entries

--- domaintools/results.py
+++ domaintools/results.py
@@ -52,17 +52,41 @@ class ParsedWhois(Results):
         parsed = self['parsed_whois']
         flat = OrderedDict()
         for key in ('domain', 'created_date', 'updated_date', 'expired_date', 'statuses', 'name_servers'):
-            value = parsed[key]
-            flat[key] = ' | '.join(value) if type(value) in (list, tuple) else value
+            if key in parsed:
+                value = parsed[key]
+                flat[key] = ' | '.join(value) if type(value) in (list, tuple) else value
 
         registrar = parsed.get('registrar', {})
         for key in ('name', 'abuse_contact_phone', 'abuse_contact_email', 'iana_id', 'url', 'whois_server'):
-            flat['registrar_{0}'.format(key)] = registrar[key]
+            if key in registrar:
+                flat['registrar_{0}'.format(key)] = registrar[key]
 
-        for contact_type in ('registrant', 'admin', 'tech', 'billing'):
-            contact = parsed.get('contacts', {}).get(contact_type, {})
-            for key in ('name', 'email', 'org', 'street', 'city', 'state', 'postal', 'country', 'phone', 'fax'):
-                value = contact[key]
-                flat['{0}_{1}'.format(contact_type, key)] = ' '.join(value) if type(value) in (list, tuple) else value
+        if "networks" in parsed:
+            networks = parsed.get("networks")
+            for network in networks:
+                id = network.get("id")
+                for key in ('range', 'asn', 'org', 'parent', 'customer', 'country', 'phone', 'status', 'source', 'updated_date', 'created_date'):
+                    if key in network:
+                        value = network[key]
+                        flat['network_{0}'.format(id)] = ' '.join(value) if type(value) in (list, tuple) else value
+
+        if "contacts" in parsed:
+            contacts = parsed.get("contacts")
+            if type(contacts) is list:
+                # handle IP-style contacts, which show up as a list
+                for contact in contacts:
+                    contact_type = contact.get("type")
+                    for key in ('name', 'email', 'org', 'abuse_mailbos', 'address', 'street', 'city', 'state', 'postal', 'country', 'phone', 'fax'):
+                        if key in contact:
+                            value = contact[key]
+                            flat['{0}_{1}'.format(contact_type, key)] = ' '.join(value) if type(value) in (list, tuple) else value
+
+            elif type(contacts) is dict:
+                for contact_type in ('registrant', 'admin', 'tech', 'billing'):
+                    contact = contacts.get(contact_type, {})
+                    for key in ('name', 'email', 'org', 'street', 'city', 'state', 'postal', 'country', 'phone', 'fax'):
+                        if key in contact:
+                            value = contact[key]
+                            flat['{0}_{1}'.format(contact_type, key)] = ' '.join(value) if type(value) in (list, tuple) else value
 
         return flat
