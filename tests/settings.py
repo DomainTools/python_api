db4c70f416b34f825314adc82aa9ea68915b91b6
Chuck Woodraska <cwoodraska@domaintools.com>
2021-10-14 11:35:59 -0700

    Fix some stuff around testing with the newer libraries.

--- tests/settings.py
+++ tests/settings.py
@@ -1,5 +1,6 @@
 """Defines all test wide settings and variables"""
 from os import environ
+from yarl import URL
 
 from domaintools import API
 from vcr import VCR
@@ -8,11 +9,19 @@ from vcr import VCR
 def remove_server(response):
     response.get('headers', {}).pop('server', None)
     if 'url' in response:
-        response.pop('url')
+        url = URL(response['url'])
+        query = dict(url.query)
+        if 'api_username' in query:
+            query.update(api_username='test')
+        if 'api_key' in query:
+            query.update(api_key='test')
+        if 'signature' in query:
+            query.update(signature='test')
+        response['url'] = str(url.with_query(query))
     return response
 
 
-vcr = VCR(before_record_response=remove_server, filter_query_parameters=['timestamp', 'signature', 'api_username'], filter_post_data_parameters=['timestamp', 'signature', 'api_username'],
+vcr = VCR(before_record_response=remove_server, filter_query_parameters=['timestamp', 'signature', 'api_username', 'api_key'], filter_post_data_parameters=['timestamp', 'signature', 'api_username', 'api_key'],
           cassette_library_dir='tests/fixtures/vcr/', path_transformer=VCR.ensure_suffix('.yaml'),
           record_mode='new_episodes')
 with vcr.use_cassette('init_user_account'):
