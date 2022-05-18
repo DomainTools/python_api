f7260aa9988ed90e89d50d6fbccc6653a5c0da36
Wesley Agena <wagena@domaintools.com>
2022-05-18 13:10:53 -1000

    remove unused changes

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,6 +1,6 @@
 from domaintools import API
 
 
-dt_api = API('wagena-api-test-user', 'eee30-51701-98ecb-89002-69d16')
-result = dt_api.iris_investigate(domains='int-chase.com').data()
+dt_api = API(USER_NAME, KEY)
+result = dt_api.iris_enrich('google.com').data()
 print(result)
