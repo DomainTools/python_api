37d156ce32f8222599be680ae681ef4db004548e
Wesley Agena <wagena@domaintools.com>
2022-05-19 09:17:41 -1000

    add function to get threat component

--- domaintools/utils.py
+++ domaintools/utils.py
@@ -12,3 +12,19 @@ def get_domain_age(create_date):
     """
     time_diff = datetime.now() - dateparser.parse(create_date)
     return time_diff.days
+
+
+def get_threat_component(components, threat_type):
+    """
+    Gets a certain threat component out a list of components
+    Args:
+        components: List of threat components
+        threat_type: Type of threat we are looking for
+
+    Returns: Either the component that we asked for or None
+    """
+    for component in components:
+        if component.get("name") == threat_type:
+            return component
+    else:
+        return None
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,3 +5,12 @@ def test_get_domain_age():
     create_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
     result = utils.get_domain_age(create_date)
     assert result == 1
+
+
+def test_get_threat_component():
+    threat_components = [
+        {"name": "threat_profile_malware", "risk_score": 31},
+        {"name": "threat_profile_spam", "risk_score": 73, "threats": ["spam"]}
+    ]
+    result = utils.get_threat_component(threat_components, "threat_profile_malware")
+    assert result.get("risk_score") == 31
