051508414b2f9f905e49dd30ea4f4df85c1227f9
JD Babac <jbabac@domaintools.com>
2025-02-14 00:07:17 +0800

    IDEV-2020: Uncomment skipped tests.

--- tests/test_api.py
+++ tests/test_api.py
@@ -564,60 +564,60 @@ def test_newly_observed_domains_feed_pagination():
     assert page_count > 1
 
 
-# @vcr.use_cassette
-# def test_newly_active_domains_feed():
-#     results = api.nad(after="-60")
-#     for response in results.response():
-#         assert results.status == 200
-#         assert response.status_code == 200
+@vcr.use_cassette
+def test_newly_active_domains_feed():
+    results = api.nad(after="-60")
+    for response in results.response():
+        assert results.status == 200
+        assert response.status_code == 200
 
-#         rows = response.text.strip().split("\n")
-#         assert response is not None
-#         assert len(rows) >= 1
+        rows = response.text.strip().split("\n")
+        assert response is not None
+        assert len(rows) >= 1
 
-#         for row in rows:
-#             feed_result = json.loads(row)
-#             assert "timestamp" in feed_result.keys()
-#             assert "domain" in feed_result.keys()
-
-
-# @vcr.use_cassette
-# def test_domainrdap_feed():
-#     results = api.domainrdap(after="-60", top=2)
-#     for response in results.response():
-#         assert results.status == 200
-#         assert response.status_code == 200
-
-#         rows = response.text.strip().split("\n")
-
-#         assert response is not None
-#         assert len(rows) == 2
-
-#         for row in rows:
-#             feed_result = json.loads(row)
-#             assert "timestamp" in feed_result.keys()
-#             assert "domain" in feed_result.keys()
-#             assert "parsed_record" in feed_result.keys()
-#             assert "domain" in feed_result["parsed_record"]["parsed_fields"]
-#             assert "emails" in feed_result["parsed_record"]["parsed_fields"]
-#             assert "contacts" in feed_result["parsed_record"]["parsed_fields"]
+        for row in rows:
+            feed_result = json.loads(row)
+            assert "timestamp" in feed_result.keys()
+            assert "domain" in feed_result.keys()
 
 
-# @vcr.use_cassette
-# def test_domain_discovery_feed():
-#     results = api.domaindiscovery(after="-60")
-#     for response in results.response():
-#         assert results.status == 200
-#         assert response.status_code == 200
+@vcr.use_cassette
+def test_domainrdap_feed():
+    results = api.domainrdap(after="-60", top=2)
+    for response in results.response():
+        assert results.status == 200
+        assert response.status_code == 200
 
-#         rows = response.text.strip().split("\n")
-#         assert response is not None
-#         assert len(rows) >= 1
+        rows = response.text.strip().split("\n")
 
-#         for row in rows:
-#             feed_result = json.loads(row)
-#             assert "timestamp" in feed_result.keys()
-#             assert "domain" in feed_result.keys()
+        assert response is not None
+        assert len(rows) == 2
+
+        for row in rows:
+            feed_result = json.loads(row)
+            assert "timestamp" in feed_result.keys()
+            assert "domain" in feed_result.keys()
+            assert "parsed_record" in feed_result.keys()
+            assert "domain" in feed_result["parsed_record"]["parsed_fields"]
+            assert "emails" in feed_result["parsed_record"]["parsed_fields"]
+            assert "contacts" in feed_result["parsed_record"]["parsed_fields"]
+
+
+@vcr.use_cassette
+def test_domain_discovery_feed():
+    results = api.domaindiscovery(after="-60")
+    for response in results.response():
+        assert results.status == 200
+        assert response.status_code == 200
+
+        rows = response.text.strip().split("\n")
+        assert response is not None
+        assert len(rows) >= 1
+
+        for row in rows:
+            feed_result = json.loads(row)
+            assert "timestamp" in feed_result.keys()
+            assert "domain" in feed_result.keys()
 
 
 @vcr.use_cassette
