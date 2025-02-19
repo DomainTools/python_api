f3a15c6f9dbc130e0a64f5e0b34c5c77d1de72dd
JD Babac <jbabac@domaintools.com>
2025-02-20 02:19:04 +0800

    IDEV-2020: Adjust cli implementation

--- domaintools_async/__init__.py
+++ domaintools_async/__init__.py
@@ -52,20 +52,9 @@ class AsyncResults(Results):
             patch_data.update(self.api.extra_request_params)
             results = await session.patch(url=self.url, json=patch_data)
         elif self.product in FEEDS_PRODUCTS_LIST:
-            parameters = deepcopy(self.kwargs)
-            parameters.pop("output_format", None)
-            parameters.pop(
-                "format", None
-            )  # For some unknownn reasons, even if "format" is not included in the cli params for feeds endpoint, it is being populated thus we need to remove it. Happens only if using CLI.
-            headers = {}
-            if self.kwargs.get("output_format", OutputFormat.JSONL.value) == OutputFormat.CSV.value:
-                parameters["headers"] = int(bool(self.kwargs.get("headers", False)))
-                headers["accept"] = HEADER_ACCEPT_KEY_CSV_FORMAT
-
-            header_api_key = parameters.pop("X-Api-Key", None)
-            if header_api_key:
-                headers["X-Api-Key"] = header_api_key
-
+            session_params = self._get_session_params()
+            parameters = session_params.get("parameters")
+            headers = session_params.get("headers")
             results = await session.get(url=self.url, params=parameters, headers=headers, **self.api.extra_request_params)
         else:
             results = await session.get(url=self.url, params=self.kwargs, **self.api.extra_request_params)
